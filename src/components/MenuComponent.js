f55a9930767d34da8f1c71a69965d44a7b4810be
Siddharth Bisht <siddharthbisht56@gmail.com>
2020-07-05 21:24:18 +0530

    presentational, container & functional components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,45 +1,35 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import DishDetail from './DishdetailComponent'
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-class Menu extends Component {
 
-  constructor(props) {
-    super(props);
+function RenderMenuItem({ dish, onClick }) {
+  return (
+    <Card onClick={()=> onClick(dish.id)}>
+      <CardImg width="100%" src={dish.image} alt={dish.name} />
+      <CardImgOverlay body className="ml-5">
+        <CardTitle><b>{dish.name}</b></CardTitle>
+      </CardImgOverlay>
+    </Card>
+  );
+}
 
-    this.state = {
-      selectedDish: null
-    }
-  }
-
-  onDishSelect(dish) {
-    this.setState({selectedDish: dish});
-  }
-
-  render() {
-
-    const menu = this.props.dishes.map((dish) => {
-      return (
-        <div key={dish.id} className="col-12 col-md-5 m-1">
-          <Card onClick={()=>this.onDishSelect(dish)}>
-            <CardImg width="100%" src={dish.image} alt={dish.name} />
-            <CardImgOverlay body className="ml-5">
-              <CardTitle><b>{dish.name}</b></CardTitle>
-            </CardImgOverlay>
-          </Card>
-        </div>
-      );
-    });
-
+const Menu = (props) => {
+  const menu = props.dishes.map((dish) => {
     return (
-      <div className="container">
-        <div className="row">
-          {menu}
-        </div>
-        <DishDetail dish={this.state.selectedDish} />
+      <div key={dish.id} className="col-12 col-md-5 m-1">
+        <RenderMenuItem dish={dish} onClick={props.onClick} />
       </div>
     );
-  }
+  });
+
+  return (
+    <div className="container">
+      <div className="row">
+        {menu}
+      </div>
+    </div>
+  );
 }
+
 
 export default Menu;
