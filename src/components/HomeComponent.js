8ee03e8cf7b7916e5bf541dc50f3f991c9840526
Siddharth Bisht <siddharthbisht56@gmail.com>
2020-07-18 10:40:46 +0530

    Redux, Client-server communication and Fetch

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -42,10 +42,10 @@ function Home(props) {
           <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishesErrMess} />
         </div>
         <div className="col-12 col-md m-1">
-          <RenderCard item={props.promotion} isLoading={props.promosLoading} errMess={props.promosErrMess}  />
+          <RenderCard item={props.promotion} isLoading={props.promosLoading} errMess={props.promosErrMess} />
         </div>
         <div className="col-12 col-md m-1">
-          <RenderCard item={props.leader} />
+          <RenderCard item={props.leader} isLoading={props.leadersLoading} errMess={props.leadersErrMess} />
         </div>
       </div>
     </div>
